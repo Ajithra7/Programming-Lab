903c1235ab8dc0870b80fcccf2378d99542087ec
Ajithra7 <79163509+Ajithra7@users.noreply.github.com>
2021-03-08 02:07:50 +0530

    Add files via upload

---
+++ Prgm_Python_LambdaFunction/LambdaFunction.py
@@ -0,0 +1,6 @@
+area_of_a_rectangle = lambda l,b : l*b
+area_of_a_square=lambda a: a*a
+area_of_a_triangle=lambda l,b: (1/2)*l*b
+print("Area of rectangle is:", area_of_a_rectangle(5,10))
+print("Area of square is:", area_of_a_square(25))
+print("Area of triangle is:", area_of_a_triangle(50,75))
